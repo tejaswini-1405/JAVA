8a2ca92f7d1b001cec6af62799e9561bdbe7f26c
Tejaswini T J <tejaswiniteju644@gmail.com>
2026-02-25 22:32:06 +0530

    Update problem3.java
    Refactored the code to remove unnecessary comments and maintain functionality.

--- Assignment/src/java_basics/problem3.java
+++ Assignment/src/java_basics/problem3.java
@@ -1,15 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+//Write a program that takes a student's marks as input and prints the corresponding
+//grade (A, B, C, D, or F) using a switch statement.
 package java_basics;
 
 import java.util.Scanner;
 
-/**
- *
- * @author ADMIN
- */
+
 public class problem3 {
 
    public static void main(String[] args) {
@@ -45,4 +40,5 @@ public class problem3 {
     }
     
 }
+
 
